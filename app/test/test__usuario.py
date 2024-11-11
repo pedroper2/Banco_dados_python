a45fa8d26a0098bafa1d2ef6280e29c2db4f7c31
Pedro Pereira Mascarenhas <161596207+pedroper2@users.noreply.github.com>
2024-11-11 20:30:00 +0000

    python

--- app/test/test__usuario.py
+++ app/test/test__usuario.py
@@ -1,15 +1,4 @@
-"""import pytest
-from app.models.usuario import Usuario
 
-
-@pytest.fixture
-def cria_usuario():
-    usuario01= Usuario("Pedro","Pedro@gmail","123")
-    return usuario01
-
-def test_varificar_valor_vazio():
-    with pytest.raises(ValueError,match= "A mensagem não pode esta vazia."):
-        Usuario("Pedro","","456")"""
 import pytest
 
 from app.models.usuario import Usuario
